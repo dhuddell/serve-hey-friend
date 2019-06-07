e81cd24da84c768ff9436050cf28bfc35c72a50b
dhuddell <dan.huddell@gmail.com>
2019-06-06 21:06:15 -0400

    merged

--- src/setters/login-user.js
+++ src/setters/login-user.js
@@ -7,17 +7,24 @@ const loginUser = async ( input ) => {
   const user = await UserModel.findOne({ username: username });
   if(!user) return { message: 'User not found.' };
 
+  console.log(user,'was found!')
   const match = await bcrypt.compare(password, user.password);
+  console.log(password ===  user.password)
   if(match) {
+    console.log('woooot')
       var token = jwt.sign({ foo: 'bar' }, 'tempi is a dog');
       return {
         message:'Success!',
         token
       };
   }
-
+  console.log('fack', match)
   // set no jwt and return failure message
   return { message: 'FAILURE, YOU HACKER!' };
 }
 
 export default loginUser;
+
+
+// $2b$10$rO6P2M.bJiPxkC3IRNNfPuaEd0eHpooRxre6LyNVfU.Fn0HQaZgEC
+// $2b$10$rO6P2M.bJiPxkC3IRNNfPuaEd0eHpooRxre6LyNVfU.Fn0HQaZgEC
