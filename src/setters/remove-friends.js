54b5d8697887e91f355caa94cc11fdeb6e389427
Dan Huddell <dan-huddell@pluralsight.com>
2020-05-31 13:57:19 -0400

    Refactor removeFriends to SQL models

--- src/setters/remove-friends.js
+++ src/setters/remove-friends.js
@@ -1,19 +1,35 @@
-import UserModel from '../schemas/user-model';
+import { UserInputError } from 'apollo-server';
 import authenticateUser from '../helpers/authenticate-user';
+import { Account, Relationship, Person, Goal } from '../sql-models';
 
-const removeFriends = ({ username }, { token }) => {
+// 5/27/2020
+// Need to create a SQL trigger to properly order and cascade deletions
+
+const removeFriends = async ({ username }, { token }) => {
   authenticateUser(username, token)
+  const account = await Account.query().where({ username }).first() || {};
+  const relationships = await Relationship.query()
+    .where({ follower_id: account.person_id });
 
-  return UserModel.findOne({ username }).then((user) => {
-    if(user) {
-      user.friends.pull({});
-      return user.save().then(() => ({
-        message: 'Removed all friends.',
-      }));
-    }
+  try {
+    const transactionResponse = Account.transaction(async (t) => {
+      try {
+        await Relationship.query().delete().where({ follower_id: account.person_id });
+        await Person.query().delete().whereIn( 'id', relationships.map((r) => r.followee_id));
+        await Goal.query().delete().whereIn('id', relationships.map((r) => r.goal_id));
 
-    throw new UserInputError('User not found');
-  });
+        return { message: `Removed friends of user '${username}'.` };
+      } catch (err) {
+        console.log('Remove friends transation error: ', err);
+        throw new UserInputError(err);
+      }
+    });
+
+    return transactionResponse;
+  } catch (error) {
+    console.log('Remove friends transation error: ', err);
+    throw new UserInputError(err);
+  }
 };
 
 export default removeFriends;
