53dc6ce93e05df239841f2579393805a9cebd9a4
Dan Huddell <dan-huddell@pluralsight.com>
2020-08-20 20:21:46 -0400

    old sql model cleanup

--- src/setters/login-user.js
+++ src/setters/login-user.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { UserInputError } from 'apollo-server';
-import { Account, Person } from '../sql-models';
+import { Account } from '../sql-models';
 
 const loginUser = async ( input ) => {
   const { username, password } = input.loginInput;
--- src/sql-models/index.js
+++ src/sql-models/index.js
@@ -1,7 +1,3 @@
-export { default as User } from './user';
-export { default as Friend } from './friend';
-export { default as GoalSet } from './goal-set';
-
 export { default as Account } from './account';
 export { default as Person } from './person';
 export { default as Goal } from './goal';
